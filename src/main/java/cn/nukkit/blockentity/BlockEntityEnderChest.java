c16b5cc0560c9a8615878ecbb471045a6b7f373c
Scallop Ye <862578730@qq.com>
2016-12-11 17:10:59 +0800

    Implements EnderChest

---
+++ src/main/java/cn/nukkit/blockentity/BlockEntityEnderChest.java
@@ -0,0 +1,52 @@
+package cn.nukkit.blockentity;
+
+import cn.nukkit.block.Block;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+
+public class BlockEntityEnderChest extends BlockEntitySpawnable implements BlockEntityNameable {
+
+    public BlockEntityEnderChest(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+
+    @Override
+    public boolean isBlockEntityValid() {
+        return this.getBlock().getId() == Block.ENDER_CHEST;
+    }
+
+    @Override
+    public String getName() {
+        return this.hasName() ? this.namedTag.getString("CustomName") : "EnderChest";
+    }
+
+    @Override
+    public boolean hasName() {
+        return this.namedTag.contains("CustomName");
+    }
+
+    @Override
+    public void setName(String name) {
+        if (name == null || name.equals("")) {
+            this.namedTag.remove("CustomName");
+            return;
+        }
+
+        this.namedTag.putString("CustomName", name);
+    }
+
+    @Override
+    public CompoundTag getSpawnCompound() {
+        CompoundTag c = new CompoundTag()
+                .putString("id", BlockEntity.ENDER_CHEST)
+                .putInt("x", (int) this.x)
+                .putInt("y", (int) this.y)
+                .putInt("z", (int) this.z);
+
+        if (this.hasName()) {
+            c.put("CustomName", this.namedTag.get("CustomName"));
+        }
+
+        return c;
+    }
+}
