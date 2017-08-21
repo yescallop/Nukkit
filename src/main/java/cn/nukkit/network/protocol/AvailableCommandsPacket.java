d8fbc19bcccc875980deaafbef272f2afd69de8b
kvetinac97 <kvetinac97@users.noreply.github.com>
2017-08-21 12:47:20 +0200

    Fix build

--- src/main/java/cn/nukkit/network/protocol/AvailableCommandsPacket.java
+++ src/main/java/cn/nukkit/network/protocol/AvailableCommandsPacket.java
@@ -6,6 +6,7 @@ import cn.nukkit.command.data.CommandParameter;
 import cn.nukkit.utils.BinaryStream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
